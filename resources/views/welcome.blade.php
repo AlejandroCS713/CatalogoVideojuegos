3fa651b06d4280cb1abe9886f58a27c888ca35f2
AlejandroCS713 <alejandro2003carbonell@gmail.com>
2025-02-06 20:56:58 +0100

    Algunos cambios para usuario logeado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,34 @@
     <!-- Header -->
     <!-- Menu -->
 
-
-
     <!-- Banner -->
     <section id="banner" class="major" style="background-image: url('{{ asset('forty/images/Designer (4).jpeg') }}'); background-size: cover; background-position: center; height: 100vh;">
         <div class="inner">
-            <header class="major">
-                <h1>¡Hello, player!</h1>
-            </header>
-            <div class="content">
-                <p>¡Find your next adventure today!</p>
-                <ul class="actions">
-                    <li><a href="{{ route('register') }}" class="button next">Start your adventure</a></li>
-                </ul>
-            </div>
+            @guest
+                <!-- Si el usuario no ha iniciado sesión-->
+
+                <header class="major">
+                    <h1>¡Hello, player!</h1>
+                </header>
+                <div class="content">
+                    <p>¡Find your next adventure today!</p>
+                    <ul class="actions">
+                        <li><a href="{{ route('register') }}" class="button next">Start your adventure</a></li>
+                    </ul>
+                </div>
+            @else
+                <!-- Si el usuario ha iniciado sesión -->
+
+                <header class="major">
+                    <h1>¡Hello, {{ Auth::user()->name }}!</h1>
+                </header>
+                <div class="content">
+                    <p>¡I'm glad to see you back here, you have to check out the new games!</p>
+                    <ul class="actions">
+                        <li><a href="{{ route('videojuegos.index') }}" class="button next">See games</a></li>
+                    </ul>
+                </div>
+            @endguest
         </div>
     </section>
 <!-- Main -->
@@ -32,7 +46,7 @@
                 <span>
                 @if ($videojuego->multimedia->isNotEmpty())
                         <!-- Si hay multimedia, muestra la imagen relacionada como fondo -->
-                        <img style="width:200px;position: relative; z-index: 2; padding-right: 20px; padding-bottom: 40px" class="imagenes" src="{{ asset($videojuego->multimedia->first()->url) }}" alt="Imagen de {{ $videojuego->nombre }}"/>
+                        <a style="background: none; border: none;cursor: pointer;" href="{{ route('videojuegos.show', $videojuego->id) }}"><img style="width:200px;position: relative; z-index: 2; padding-right: 20px; padding-bottom: 40px" class="imagenes" src="{{ asset($videojuego->multimedia->first()->url) }}" alt="Imagen de {{ $videojuego->nombre }}"/></a>
                     @else
 
                     @endif
@@ -50,12 +64,12 @@
         <section id="two">
             <div class="inner">
                 <header class="major">
-                    <h2>Join our community of players</h2>
+                    <h2>Comment on the forum and talk with other users!</h2>
                 </header>
-                <p>Create your account and unlock the full experience: save your favorite games, share your opinions in the forums and connect with other gaming enthusiasts. Start being part of something epic today!
+                <p>Welcome back! You're ready to explore even more - keep saving your favorite games, participate in forum discussions, and connect with other players. You're part of an amazing community, and the adventure just keeps getting better
                 </p>
                 <ul class="actions">
-                    <li><a href="{{ route('register') }}" class="button next">Start your adventure</a></li>
+                    <li><a href="{{ route('foro.index') }}" class="button next">Go to the forum</a></li>
                 </ul>
             </div>
         </section>
